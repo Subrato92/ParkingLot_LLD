1087c82715ce8d82da18bb32a526d868edbad37b
Subrato92 <subrato.mondal18@gmail.com>
2023-02-19 22:42:27 +0530

    Msg

--- src/main/java/com/projects/parkinglot/ParkinglotApplication.java
+++ src/main/java/com/projects/parkinglot/ParkinglotApplication.java
@@ -12,6 +12,9 @@ public class ParkinglotApplication {
 
 	/**
 	 * java -cp /home/oem/Documents/Projects/jars/h2-2.1.214.jar org.h2.tools.Shell -url "jdbc:h2:mem:test" -driver "org.h2.Driver" -user "" -password ""
+	 * git config user.email "subrato.mondal18@gmail.com"
+	 * git config user.name "Subrato92"
+	 * ssh-keygen -t rsa -b 4096 -C "subrato.mondal18@gmail.com"
 	 */
 
 }
